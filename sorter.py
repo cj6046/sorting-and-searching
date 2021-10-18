2624c42f3040f9b9fd7d9ab0d4db67cd2c9da046
cj6046 <cdjones6046@gmail.com>
2021-10-18 10:35:19 -0400

    Updated sorter.py
    Added merge sort implementation and correct comments

--- sorter.py
+++ sorter.py
@@ -1,50 +1,102 @@
+"""This module holds functions for sorting lists of integers in various ways.
+
+This module has functionality for:
+selection sort
+"""
 import random
 
-def printIntro():
+def print_intro():
     print("""----------------------------------------------------------
 ----------Welcome to Sorting Algorithm Practice!----------
 ----------------------------------------------------------""")
     print("""Please choose an option: 
     1: Selection sort
+    2: Merge sort
     0: Quit""")
 
-def getLength():
+
+def get_length():
+    """Get the length of list from the user."""
     length = int(input("How long is the list? "))
     return length
 
-def getRandomList(length):
-    return random.sample(range(0, 100), length)
+def get_random_list(length):
+    """Return a list of integers between 0 and 100."""
+    return random.sample(range(0, 101), length)
 
-def selectionSort(l, i):
-    if i >= len(l):
-        return l
+def selection_sort(my_list, i):
+    """Sort a list of integers in increasing order via selection sort."""
+    if i >= len(my_list):
+        return my_list
     else:
-        swap(l, i, getMinPos(l, i))
-        return selectionSort(l, i + 1)
+        swap(my_list, i, get_min_pos(my_list, i))
+        return selection_sort(my_list, i + 1)
 
-def swap(l, a, b):
-    temp = l[a]
-    l[a] = l[b]
-    l[b] = temp
+def swap(my_list, a, b):
+    """Swap two elements in a list."""
+    temp = my_list[a]
+    myList[a] = my_list[b]
+    myList[b] = temp
 
-def getMinPos(l, i):
-    smallest = l[i]
-    for num in l[i:]:
+def get_min_pos(my_list, i):
+    """Return the position of the smallest integer in a lits."""
+    smallest = my_list[i]
+    for num in my_list[i:]:
         if num < smallest:
             smallest = num
-    return l.index(smallest, i, len(l))
+    return my_list.index(smallest, i, len(my_list))
+
+def merge_sort(my_l):
+    """Sort a list of integers in increasing order via merge sort."""
+    if(len(my_l) == 1):
+        return my_l
+    first_half = my_l[:int(len(my_l) / 2)]
+    second_half = my_l[int(len(my_l) / 2):]
+    # print("The first half of the array is: " + str(first_half))
+    # print("The second half of the array is: " + str(second_half))
+    merge_sort(first_half)
+    merge_sort(second_half)
+    merge(my_l, first_half, second_half)
     
-printIntro()
+
+def merge(whole_list, first_list, second_list):
+    """Merge two sorted lists into one sorted list."""
+    first_i = 0
+    second_i = 0
+    i = 0
+    while first_i < len(first_list) and second_i < len(second_list):
+        if(first_list[first_i] < second_list[second_i]):
+            whole_list[i] = first_list[first_i]
+            first_i += 1
+        else:
+            whole_list[i] = second_list[second_i]
+            second_i += 1
+        i += 1
+    while first_i < len(first_list):
+        whole_list[i] = first_list[first_i]
+        first_i += 1
+        i += 1
+    while second_i < len(second_list):
+        whole_list[i] = second_list[second_i]
+        second_i += 1
+        i += 1
+    return whole_list
+    
+print_intro()
 while True:
     response = int(input())
     if response == 1:
         print("You chose selection sort")
-        myList = getRandomList(getLength())
-        print("The Array is: " + str(myList))
-        selectionSort(myList, 0)
-        print("After the array is sorted: " + str(myList))
+        my_list = get_random_list(get_length())
+        print("The array is: " + str(my_list))
+        selection_sort(my_list, 0)
+        print("After the array is sorted: " + str(my_list))
     elif response == 2:
         print("You chose merge sort")
+        my_list = get_random_list(get_length())
+        print("The array is: " + str(my_list))
+        merge_sort(my_list)
+        print("After the array is sorted: " + str(my_list))
     else:
         print("Goodbye!")
         break
